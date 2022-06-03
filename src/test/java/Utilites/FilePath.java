29d8080dc7760de1628537a1e32bcf34a66771ae
Anjaneya <guntaanjaneya@gmail.com>
2022-06-03 11:26:03 -0700

    first commit

---
+++ src/test/java/Utilites/FilePath.java
@@ -0,0 +1,11 @@
+package Utilites;
+
+public interface FilePath {
+
+	
+	public static String ChromePath ="C:\\automation\\sdet_qe_automation_assignment_05\\Driver\\chromedriver.exe";
+	public static String PropertiesPath ="C:\\automation\\sdet_qe_automation_assignment_05\\src\\test\\resources\\ConfigFiles\\Config.properties";
+	public static String XLPath ="C:\\automation\\sdet_qe_automation_assignment_05\\src\\test\\resources\\ExcelFiles\\Testxl.xlsx";
+	
+
+}
